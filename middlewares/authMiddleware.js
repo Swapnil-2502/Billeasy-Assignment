07e71bcb4b93b3705b7afb662b6f11fccd368bd7
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2025-06-06 16:57:40 +0530

    AuthMiddleware added

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authMiddleware = (req,res,next) => {
+    const authHeader = req.headers.authentication
+
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).json({message: "Authorization token required"})
+    }
+
+    const token = authHeader.split(' ')[1]; 
+
+    try{
+
+        const decoded = jwt.verify(token,JWT_SECRET)
+        req.user = { userId: decoded.userId }; 
+
+        next();
+
+    }
+    catch(error){
+        res.status(401).json({message: 'Invalid or expired token'})
+    }
+}
+
+module.exports = {
+    authMiddleware
+}
